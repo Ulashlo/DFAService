0b26a0b17a0c992362d8707842560fe667545e1c
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2022-03-03 00:10:22 +0300

    init

---
+++ DFABackend/src/main/java/com/hse/dfa/backend/DfaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hse.dfa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DfaBackendApplication.class, args);
+    }
+
+}
---
+++ DFABackend/src/test/java/com/hse/dfa/backend/DfaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hse.dfa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
