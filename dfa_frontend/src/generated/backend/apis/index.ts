c5db251ab5fce56af9b18b85081fdee283629e69
Nikolskiy Vlad <Nikolskiy.2000@mail.ru>
2022-05-01 18:01:53 +0300

    Сгенерировал api

--- dfa_frontend/src/generated/backend/apis/index.ts
+++ dfa_frontend/src/generated/backend/apis/index.ts
@@ -1,4 +1,5 @@
 export * from './AuthControllerApi';
 export * from './DfaControllerApi';
 export * from './ExchangerControllerApi';
+export * from './RequestControllerApi';
 export * from './UserControllerApi';
